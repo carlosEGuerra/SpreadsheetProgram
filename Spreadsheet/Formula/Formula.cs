8cc900d9107e9b6a13747468497d4c4d7d4af353
Ellen Brigance <ebrigance@hotmail.com>
2017-01-25 23:19:53 -0700

    Finishing all conditions to check valid formula.

--- Spreadsheet/Formula/Formula.cs
+++ Spreadsheet/Formula/Formula.cs
@@ -1,5 +1,5 @@
 ﻿// Skeleton written by Joe Zachary for CS 3500, January 2017
-//Testing
+// Appended by Ellen Brigance, January 25, 2017
 
 using System;
 using System.Collections.Generic;
@@ -38,7 +38,147 @@ namespace Formulas
         /// </summary>
         public Formula(String formula)
         {
+            //Check for tokens.
+            //No tokens, throw FormulaFormatException.
+            if (formula == null || formula == "")
+            {
+                throw new FormulaFormatException("No tokens given, formula format is invalid.");
+            }
+
+            //Deal with the formula, using evaluation of stacks.
+            IEnumerable<string> s;
+            Stack<string> formStack = new Stack<string>();
+            for (int i = 0; i < formula.Length; i++)
+            {
+                s = GetTokens(formula);//Convert string to tokens, one at a time. Deal with them on the stacks accordingly.
+
+                string token = s.ToString();
+                //string topStack = "";
+                //if(formStack.Count > 0)//Hold the value at the top of the stack.
+                //{
+                //   topStack = formStack.Peek();
+                //}
+
+                double output;
+                if (Double.TryParse((token), out output) && output < 0)//Check for negative numbers
+                {
+                    throw new FormulaFormatException("Invalid token found, negative numbers not allowed.");
+                }
+                
+                //CHECKS FOR INVALID CHARACTERS (not an operator, a variable, or an open/closed paren)
+                if(!isOperator(token) && !(output > 0) && !Char.IsLetter(token[0]) && token != "(" && token != ")")
+                {
+                    throw new FormulaFormatException("Invalid token. Symbol not allowed.");
+                }
+                //IF WE HAVE A DOUBLE.
+                if (formStack.Count == 0 && output >= 0)
+                {
+                    formStack.Push(token);
+                    continue;
+                }
+                if (formStack.Count > 0 && output >= 0 && (formStack.Peek() == "/" || formStack.Peek() == "*"
+                    || formStack.Peek() == "+" || formStack.Peek() == "-"))
+                {
+                    formStack.Pop();//Pop the operator and operand on the stack.
+                    formStack.Pop();
+                    continue;
+                }
+                if (formStack.Count > 0 && output >= 0
+                    && (formStack.Peek() != "(") || !isOperator(formStack.Peek()))//No paren/operand on stack.
+                {
+                    throw new FormulaFormatException("Formula format invalid. Two operands in sequence.");
+                }
+                //IF WE HAVE AN OPEN PARENTESES.
+                if (formStack.Count == 0 && token == "(")//Stack empty.
+                {
+                    formStack.Push(token);
+                    continue;
+                }
+                if (formStack.Count > 0 && token == "(" && (formStack.Peek() == "("
+                    || isOperator(formStack.Peek())))//Peek is a paren or operand.
+                {
+                    formStack.Push(token);
+                    continue;
+                }
+                if (formStack.Count > 0 && token == "(" && (!isOperator(formStack.Peek())
+                    || formStack.Peek() != "("))
+                {
+                    throw new FormulaFormatException("Formula invalid. Open parentheses adjacent to operand.");
+                }
+                //IF WE HAVE A CLOSED PARENTHESES.
+                if (formStack.Count == 0 && token == ")")
+                {
+                    throw new FormulaFormatException("Formula invalid. Too many closing parenteses.");
+                }
+                if (formStack.Count > 0 && token == ")") //If the stack is full, pop until we see a "("
+                {
+                    while (formStack.Count > 0)
+                    {
+                        if (formStack.Peek() != "(")
+                        {
+                            formStack.Pop();
+                        }
+                    }
+                    if (formStack.Count == 0)//We never saw a "("
+                    {
+                        throw new FormulaFormatException("Formula invalid. Too many closing parenteses.");
+                    }
+                    if (formStack.Peek() == "(")//We found a "("
+                    {
+                        formStack.Pop();
+                        continue;
+                    }
+                }
+                //IF WE HAVE AN OPERATOR.
+                if (isOperator(token) && formStack.Count == 0)
+                {
+                    throw new FormulaFormatException("Formula invalid. Operator without operands.");
+                }
+                if (isOperator(token) && formStack.Count > 0 && isOperator(formStack.Peek()))//Stack top is also operand.
+                {
+                    throw new FormulaFormatException("Formula invalid. Two adjacent operands.");
+                }
+                if (isOperator(token) && formStack.Count > 0 && formStack.Peek() == "(")
+                {
+                    throw new FormulaFormatException("Formula invalid. Operator without operand.");
+                }
+                if (isOperator(token) && formStack.Count > 0)//At this point, we assume we have a valid operand.
+                {
+                    formStack.Push(token);
+                    continue;
+                }
+                //IF WE HAVE A VARIABLE.
+                if(formStack.Count == 0 && Char.IsLetter(token[0]))
+                {
+                    formStack.Push(token);
+                    continue;
+                }
+                if(Char.IsLetter(token[0]) && formStack.Count > 0 && (isOperator(formStack.Peek()) || formStack.Peek() == "("))//Have an operator or an open paren.
+                {
+                    formStack.Push(token);
+                    continue;
+                }
+                if(Char.IsLetter(token[0]) && formStack.Count > 0 && (!isOperator(formStack.Peek()) || formStack.Peek() != "("))//Have no operator or open paren.
+                {
+                    throw new FormulaFormatException("Formula invalid. Two operands adjacent.");
+                }
+            }
         }
+
+        /// <summary>
+        /// Returns true if we are looking at a valid operand. False, otherwise.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private bool isOperator(string s)
+        {
+            if (s == "/" || s == "*" || s == "+" || s == "-")
+            {
+                return true;
+            }
+            return false;
+        }
+
         /// <summary>
         /// Evaluates this Formula, using the Lookup delegate to determine the values of variables.  (The
         /// delegate takes a variable name as a parameter and returns its value (if it has one) or throws
@@ -52,6 +192,7 @@ namespace Formulas
         {
             return 0;
         }
+
 
         /// <summary>
         /// Given a formula, enumerates the tokens that compose it.  Tokens are left paren,
@@ -128,6 +269,7 @@ namespace Formulas
         /// </summary>
         public FormulaFormatException(String message) : base(message)
         {
+
         }
     }
 
