312621d7119cfe6416b88d873780c8d5ad02ff8a
u0847821 <u0847821@utah.edu>
2017-03-07 16:00:56 -0700

    Added a base gui

--- Spreadsheet/SpreadsheetVIEW/Form1.Designer.cs
+++ Spreadsheet/SpreadsheetVIEW/Form1.Designer.cs
@@ -1,13 +1,13 @@
-﻿namespace SSGui
+﻿namespace SpreadsheetVIEW
 {
-    partial class SpreadsheetPanel
+    partial class Form1
     {
-        /// <summary> 
+        /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
+        /// <summary>
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,22 +20,23 @@
             base.Dispose(disposing);
         }
 
-        #region Component Designer generated code
+        #region Windows Form Designer generated code
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
+        /// <summary>
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.SuspendLayout();
             // 
-            // SpreadsheetPanel
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "SpreadsheetPanel";
-            this.Size = new System.Drawing.Size(272, 258);
+            this.ClientSize = new System.Drawing.Size(976, 936);
+            this.Name = "Form1";
+            this.Text = "Ghetto Excel";
             this.ResumeLayout(false);
 
         }
@@ -43,3 +44,4 @@
         #endregion
     }
 }
+
