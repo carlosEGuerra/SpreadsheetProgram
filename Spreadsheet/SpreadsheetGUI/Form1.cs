7ae00ff6799aa79a1179cb4230e003cfd32a29a6
u0847821 <u0847821@utah.edu>
2017-03-08 09:04:04 -0700

    Most of the GUI is now complete, need to do the methods

--- Spreadsheet/SpreadsheetGUI/Form1.cs
+++ Spreadsheet/SpreadsheetGUI/Form1.cs
@@ -16,5 +16,25 @@ namespace SpreadsheetGUI
         {
             InitializeComponent();
         }
+
+        private void spreadsheetPanel1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Need To Identify which arrow key is pressed and change the view accordingly
+        private void spreadsheetPanel1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        //Should Display the currently selected Cell Name
+        private void cellName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cellContent_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
